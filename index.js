4a5ac7fdedfe7ebc4441b8d36541d8c851b9dd3a
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-06 09:01:26 -0400

    Add first and last name

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function handleSubmit(event) {
     event.preventDefault();
     const form = event.target;
     const heading = document.querySelector('h1');
-    heading.textContent = form.personName.value;
+    const fullName = form.firstName.value + " " + form.lastName.value;
+    heading.textContent = fullName;
 }
 
 personForm.addEventListener('submit',handleSubmit);
