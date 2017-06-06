daf6a49900e1b03637b7b4f0f648aaec22cf9a40
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-06 14:39:56 -0400

    add list of details

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function handleSubmit(event) {
 
     const fullName = form.firstName.value + ' ' + form.lastName.value;
 
-    const boldedName = document.createElement('strong');
-    boldedName.textContent = fullName;
+    // const boldedName = document.createElement('strong');
+    // boldedName.textContent = fullName;
 
-    details.appendChild(boldedName);
+    // details.appendChild(boldedName);
 
     const favCol = form.favoriteColor.value;
     const comCol = form.secondFavoriteColor.value;
@@ -25,6 +25,16 @@ function handleSubmit(event) {
     const favFood = form.favoriteFood.value;
     const introText = document.querySelector('#introText');
     introText.textContent = 'Hello, I like ' + favFood + ' and my favorite colors are shown on this page.';
+
+    const age = form.age.value;
+
+    details.innerHTML = `
+    <ul>
+        <li>Name: ${fullName}</li>
+        <li>Favorite Color: ${favCol}</li>
+        <li>Age: ${age}</li>
+    </ul>
+    `;
 }
 
 personForm.addEventListener('submit',handleSubmit);
