b4d7fcf4c413939cd2648a9b2e01b3037c804e50
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-06 09:16:45 -0400

    Add favorite color and food

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ function handleSubmit(event) {
     event.preventDefault();
     const form = event.target;
     const heading = document.querySelector('h1');
-    const fullName = form.firstName.value + " " + form.lastName.value;
+    const fullName = form.firstName.value + ' ' + form.lastName.value;
+
+    const favCol = form.favoriteColor.value;
+    const comCol = form.secondFavoriteColor.value;
+    
+    document.body.style.backgroundColor = favCol;
+    document.body.style.color = comCol;
+
     heading.textContent = fullName;
+
+    const favFood = form.favoriteFood.value;
+    const introText = document.querySelector('#introText');
+    introText.textContent = 'Hello, I like ' + favFood + ' and my favorite colors are shown on this page.';
 }
 
 personForm.addEventListener('submit',handleSubmit);
